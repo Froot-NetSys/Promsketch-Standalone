b6a689cb6f5d59d8fb5030a7341a9253c951f16b
Kenny Wu <43484854+KennyWu@users.noreply.github.com>
2024-06-19 00:03:02 -0700

    change script config

--- ExporterStarter/startup.sh
+++ ExporterStarter/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 fake_norm_exporter.py --port=10000 --instancestart=0 --batchsize=100 --valuescale=10 & 
-./victoria-metrics --promscrape.config="vmconfig.yml" &
+./victoria-metrics --promscrape.config="num_samples_config.yml" &
 ./vmalert -rule="rules.yml" -datasource.url="http://localhost:8428" -remoteWrite.url="http://localhost:8428" -evaluationInterval=20s &
 
 echo "created apps"
